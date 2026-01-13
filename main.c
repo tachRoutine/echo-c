3a877bec055ff1f28e692a1e058c3f9ce024fabf
tacheraSasi <tacherasasi@gmail.com>
2026-01-14 00:35:36 +0300

    add the basic logic using wroite instead of printf

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv){
+    for (int i = 1; i < argc; i++) {
+        write(1, argv[i], strlen(argv[i]));
+        
+        if (i < argc - 1){
+            write(1," ",1);
+        }
+    }
+    
+    write(1,"\n",1);
+    return 0;
+}
