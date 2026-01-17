f6561b885cd0813abb3ef5d8c753435af7c79160
tacheraSasi <tacherasasi@gmail.com>
2026-01-17 23:03:57 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,12 +9,28 @@ struct Options {
 
 int main(int argc, char **argv) {
   int arg_start_index = 1;
-  struct Options default_options = {true, false};
+  struct Options options = {true, false};
 
   while (arg_start_index < argc) {
     // I am making assumptions that options can only be 2 chars
     if (strlen(argv[arg_start_index]) != 2 && argv[arg_start_index][0] != '-') {
       continue;
+    }
+    
+    // Using normal if statements here 
+    // since i think all these flags can all exsit at once
+    // like echo something -e -n ...
+    // i will read the original source code toknow more
+    if (argv[arg_start_index][1] == 'n'){
+        options.print_newline = true;
+    }
+    
+    if (argv[arg_start_index][1] == 'e'){
+        options.interpret_escapes = true;
+    }
+    
+    if (argv[arg_start_index][1] == 'E'){
+        options.interpret_escapes = false;
     }
 
     arg_start_index++;
@@ -28,7 +44,7 @@ int main(int argc, char **argv) {
     }
   }
 
-  if (default_options.print_newline) {
+  if (options.print_newline) {
     write(1, "\n", 1);
   }
   return 0;
