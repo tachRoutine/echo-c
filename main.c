b613f64a26fbeebe5e7e939f36d684491771200b
tacheraSasi <tacherasasi@gmail.com>
2026-01-17 22:58:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,35 @@
-#include <string.h>
-#include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 
 struct Options {
-    bool print_newline;
-    bool interpret_escapes;
+  bool print_newline;
+  bool interpret_escapes;
 };
 
-int main(int argc, char **argv){
-    int arg_start_index = 1;
-    struct Options default_options = {true,false};
-    
-    while(arg_start_index < argc){
-        if(strlen(argv[arg_start_index]) != 2 && argv[arg_start_index][0] != "-"){
-            continue;
-        }
-        
-        arg_start_index++;
+int main(int argc, char **argv) {
+  int arg_start_index = 1;
+  struct Options default_options = {true, false};
+
+  while (arg_start_index < argc) {
+    // I am making assumptions that options can only be 2 chars
+    if (strlen(argv[arg_start_index]) != 2 && argv[arg_start_index][0] != '-') {
+      continue;
     }
 
-    for (int i = 1; i < argc; i++) {
-        write(1, argv[i], strlen(argv[i]));
-        
-        if (i < argc - 1){
-            write(1," ",1);
-        }
+    arg_start_index++;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    write(1, argv[i], strlen(argv[i]));
+
+    if (i < argc - 1) {
+      write(1, " ", 1);
     }
-    
-    if(default_options.print_newline){
-        write(1,"\n",1);
-    }
-    return 0;
+  }
+
+  if (default_options.print_newline) {
+    write(1, "\n", 1);
+  }
+  return 0;
 }
