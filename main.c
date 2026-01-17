4efae99bad6e670d8d08a914ef88781769f2f644
tacheraSasi <tacherasasi@gmail.com>
2026-01-17 22:56:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <string.h>
 
 struct Options {
     bool print_newline;
@@ -10,6 +11,14 @@ struct Options {
 int main(int argc, char **argv){
     int arg_start_index = 1;
     struct Options default_options = {true,false};
+    
+    while(arg_start_index < argc){
+        if(strlen(argv[arg_start_index]) != 2 && argv[arg_start_index][0] != "-"){
+            continue;
+        }
+        
+        arg_start_index++;
+    }
 
     for (int i = 1; i < argc; i++) {
         write(1, argv[i], strlen(argv[i]));
